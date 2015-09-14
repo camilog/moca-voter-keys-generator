8256eed76f8e651ad91c2d69dfbdbf4ddb390c07
Camilo Gomez <cjgomez@autistici.org>
2015-09-14 14:07:13 -0300

    Seteo del bb-address se despliega en pantalla principal

--- src/MainWindowController.java
+++ src/MainWindowController.java
@@ -3,6 +3,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,13 +13,32 @@ import java.io.IOException;
 public class MainWindowController {
 
     @FXML
-    public void handleConfigureBBAddressButtonAction(ActionEvent actionEvent) throws IOException {
+    protected TextField new_bulletin_board_address;
 
-        Parent root = FXMLLoader.load(getClass().getResource("configWindow.fxml"));
-        Stage stage = new Stage();
-        stage.setTitle("Configure Bulletin Board Address");
-        stage.setScene(new Scene(root, 400, 200));
-        stage.show();
+    @FXML
+    protected Button change_bb_button;
+
+    @FXML
+    protected Label bulletin_board_label;
+
+    @FXML
+    public void handleDisplayChangeBBAddressButtonAction(ActionEvent actionEvent) throws IOException {
+
+        new_bulletin_board_address.setVisible(true);
+        change_bb_button.setVisible(true);
+
+    }
+
+    @FXML
+    public void handleChangeBBAddressButtonAction(ActionEvent actionEvent) {
+
+        String newBBAddress = new_bulletin_board_address.getText();
+
+        GenerateKeys.setBBAddress(newBBAddress);
+
+        bulletin_board_label.setText(GenerateKeys.getBBAddress());
+        change_bb_button.setVisible(false);
+        new_bulletin_board_address.setVisible(false);
 
     }
 
